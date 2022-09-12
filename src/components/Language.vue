1acd539a02df295f02b6be516632c18f02f154ac
barry <1035102183@qq.com>
2022-09-12 21:37:02 +0800

    feat: 抽离语言选择组件

---
+++ src/components/Language.vue
@@ -0,0 +1,32 @@
+<script setup lang='ts'>
+import useLocale from '~/hooks/locale'
+defineProps<{
+  size?: number
+}>()
+const { changeLocale } = useLocale()
+const languageList = ref([
+  { label: '中文', value: 'zh-CN' },
+  { label: 'English', value: 'en-US' },
+])
+const changeLanguage = (command: string | number | object) => {
+  changeLocale(command as string)
+}
+</script>
+
+<template>
+  <el-dropdown @command="changeLanguage">
+    <icon-ant-design-translation-outlined :style="{ fontSize: `${size}em` }" />
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item
+          v-for="(item, index) of languageList" :key="index"
+          :command="item.value"
+        >
+          {{ item.label }}
+        </el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+</template>
+
+<style scoped></style>
